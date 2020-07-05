4122af7adf7bb1e161e83b3bd241f0108456a491
Edgar Ong <emailedgar@gmail.com>
2020-07-05 11:33:02 -0400

    (feat): add related posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,41 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Img from "gatsby-image";
 
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import { SimilarArticlesFactory } from "../utils";
 
-export default function BlogPost({ data }) {
-  const { title, body, heroImage, description, updatedAt, author } = data.contentfulBlogPost;
+export default function BlogPost({ data }) { 
+  const { title, body, heroImage, description, updatedAt, author, slug, tags } = data.contentfulBlogPost;
   const dateFormat = { year: 'numeric', month: 'long', day: 'numeric' }
   const parsedDate = new Date(updatedAt).toLocaleDateString([], dateFormat);
+
+  const articles = data.allContentfulBlogPost.nodes;
+  const similarArticles = new SimilarArticlesFactory(
+    articles, slug
+  )
+    .setMaxArticles(4)
+    .setTags(tags)
+    .getArticles()
+
+  const renderSimilarArticles = () => {
+    const listArticles = similarArticles.map(article => {
+      const { title, description, slug } = article.article;
+      return (
+        <React.Fragment key={title}>
+          <Link to={`/${slug}`}><h4>{title}</h4></Link>
+          <p className="small">{description.description}</p>
+        </React.Fragment>
+      )
+    });
+
+    return <div id="related-posts" className="related-posts">
+      <hr />
+      <h3 className="mb-4 mt-4">Recommended Reading</h3>
+      {listArticles}
+    </div>
+  }
+
   return <Layout title={title} description={description.description} imgUrl={heroImage.fluid.src} page={true}>
     <div id="hero" className="hero row text-light p-4 ml-0 mr-0">
       <div className="text-center">
@@ -16,12 +44,13 @@ export default function BlogPost({ data }) {
       </div>
     </div>
     <div className="container mb-4 mt-4 pt-4 pb-4">
-      <div className="article">
+      <div id="article-body" className="article">
         <div className="mb-4">
           <Img fluid={heroImage.fluid} alt={title} className="w-100 rounded-lg" />
         </div>
         <div dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}/>
       </div>
+      {renderSimilarArticles()}
     </div>
   </Layout> 
 }
@@ -50,6 +79,18 @@ export const query = graphql`
       author {
         name
       }
+      slug
+      tags
     }
+    allContentfulBlogPost {
+    nodes {
+      tags
+      title
+      description {
+        description
+      }
+      slug
+    }
+  }
   }
 `
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { includes, orderBy } from 'lodash'
+
 import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 export const OutboundLinkGA = props => {
@@ -61,3 +63,90 @@ export const linkedinIcon = (
     <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
   </svg>
 )
+
+// SimilarArticlesFactory.js
+// https://khalilstemmler.com/articles/gatsby-related-posts-component/
+export class SimilarArticlesFactory {
+  // (1.) Create by passing in articles, currentSlug
+  constructor (articles, currentArticleSlug) {
+    // (2.) Don't include the current article in articles list
+    this.articles = articles.filter(
+      (aArticle) => aArticle.slug !== currentArticleSlug);
+
+    this.currentArticleSlug = currentArticleSlug;
+    // (3.) Set default values
+    this.maxArticles = 3;
+    this.tags = []
+  }
+
+  // (4.) Builder pattern usage
+  setMaxArticles (m) {
+    this.maxArticles = m;
+    return this;
+  }
+
+  setTags (tagsArray) {
+    this.tags = tagsArray;
+    return this;
+  }
+
+  getArticles () {
+    const { tags, articles, maxArticles } = this;
+    // (5.) We use an Identity Map to keep track of score
+    const identityMap = {};
+
+    if (!!tags === false || tags.length === 0) {
+      console.error('SimilarArticlesFactory: Tags not provided, use setTags().')
+      return [];
+    }
+
+    function getSlug (article) {
+      return article.slug;
+    }
+
+    function addToMap (article) {
+      const slug = getSlug(article);
+      if (!identityMap.hasOwnProperty(slug)) {
+        identityMap[slug] = {
+          article: article,
+          points: 0
+        }
+      }
+    }
+
+    // (7.) For tags matches, we add 1 point
+    function addTagsPoints (article, tags) {
+      const tagPoint = 1;
+      const slug = getSlug(article);
+      
+      article.tags.forEach((aTag) => {
+        if (includes(tags, aTag)) {
+          identityMap[slug].points += tagPoint;
+        }
+      })
+    }
+
+    function getIdentityMapAsArray () {
+      return Object.keys(identityMap).map((slug) => identityMap[slug]);
+    }
+    
+    // (6.) Map over all articles, add to map and add points
+    for (let article of articles) {
+      addToMap(article);
+      addTagsPoints(article, tags)
+    }
+    
+    // (8.) Convert the identity map to an array
+    const arrayIdentityMap = getIdentityMapAsArray();
+
+    // (9.) Use a lodash utility function to sort them 
+    // by points, from greatest to least
+    const similarArticles = orderBy(
+      arrayIdentityMap, ['points'], ['desc']
+    )
+
+    // (10. Take the max number articles requested)
+    return similarArticles.splice(0, maxArticles);
+  }
+}
+
